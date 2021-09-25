beb30470e0a071cb7b8238ea80366fb571d8be6a
D. Fyffe <fyffed@netzero.net>
2021-09-25 12:31:02 -0400

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1202,7 +1202,7 @@ static void ui_draw_vision_bsd_right(UIState *s) {
 
 // tpms display by openpilotusers
 static void ui_draw_tpms(UIState *s) {
-  int tpms_x = s->fb_w - 260;
+  int tpms_x = s->fb_w - 240;
   int tpms_y = 684;
   int tpms_w = 180;
   int tpms_h = 160;
